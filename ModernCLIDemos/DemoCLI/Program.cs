b50287b521e6d7679de0a284f9f623902e6d1922
Alex Thissen <athissen@xpirit.com>
2021-07-19 21:48:52 +0200

    Added skeleton application

---
+++ ModernCLIDemos/DemoCLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+
+namespace DemoCLI
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            RootCommand rootCommand = new RootCommand("Steam Command-line Interface");
+
+            // Show commandline help unless a subcommand was used.
+            rootCommand.Handler = CommandHandler.Create<IHelpBuilder>(help =>
+                {
+                    help.Write(rootCommand);
+                    return 1;
+                });
+            var builder = new CommandLineBuilder(rootCommand).UseDefaults();
+
+            var parser = builder.Build();
+            return await parser.InvokeAsync(args);
+        }
+    }
+}
